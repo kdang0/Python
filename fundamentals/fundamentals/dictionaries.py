89dd120c90d82db6dc5895a2f3ada33c50872475
kdang0 <73298064+kdang0@users.noreply.github.com>
2022-09-26 17:52:11 -0400

    Nested_Dictionaries_Lists

---
+++ fundamentals/fundamentals/dictionaries.py
@@ -0,0 +1,90 @@
+x = [[5, 2, 3], [10, 8, 9]]
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'}
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [{'x': 10, 'y': 20}]
+
+x[1][0] = 15
+print(x)
+
+students[0]["last_name"] = "Bryant"
+print(students)
+
+sports_directory["soccer"][0] = "Andres"
+print(sports_directory)
+
+z[0]["y"] = 30
+print(z)
+
+
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+def iterateDictionary(some_list):
+    for x in range(0,len(some_list)):
+        for key, val in some_list[x].items():
+            print(f"{key} - {val}", end=", ")
+        print()
+
+
+iterateDictionary(students)
+
+def iterateDictionary2(key_name, some_list):
+    for x in range(0, len(some_list)):
+        print(f"{some_list[x][key_name]}")
+
+iterateDictionary2("first_name", students)
+iterateDictionary2("last_name", students)
+
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
+def printInfo(some_dict):
+    for property in some_dict:
+        print(f"{len(some_dict[property])} {property.upper()}")
+        for element in some_dict[property]:
+            print(element)
+        print()
+
+printInfo(dojo)
